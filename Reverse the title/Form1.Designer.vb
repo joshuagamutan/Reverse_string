4cfe3dd1df9c0e4998add4073d7b2f30ec4346c7
joshu <joshu@ALIMBOYONG>
2023-10-22 18:40:55 +0800

    Add project files.

---
+++ Reverse the title/Form1.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ll
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.lbel1 = New System.Windows.Forms.Label()
+        Me.lbel2 = New System.Windows.Forms.Label()
+        Me.lbel3 = New System.Windows.Forms.Label()
+        Me.txt4 = New System.Windows.Forms.TextBox()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.txt5 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(48, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 37)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Title 1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(159, 75)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(233, 42)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Reverse "
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(159, 32)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(233, 37)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(48, 159)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 37)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Title 2"
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(39, 278)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(89, 37)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Title 3"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(159, 154)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(233, 37)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(159, 278)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(233, 37)
+        Me.TextBox3.TabIndex = 6
+        '
+        'lbel1
+        '
+        Me.lbel1.AutoSize = True
+        Me.lbel1.Location = New System.Drawing.Point(431, 51)
+        Me.lbel1.Name = "lbel1"
+        Me.lbel1.Size = New System.Drawing.Size(60, 17)
+        Me.lbel1.TabIndex = 7
+        Me.lbel1.Text = "Result 1"
+        '
+        'lbel2
+        '
+        Me.lbel2.AutoSize = True
+        Me.lbel2.Location = New System.Drawing.Point(431, 159)
+        Me.lbel2.Name = "lbel2"
+        Me.lbel2.Size = New System.Drawing.Size(60, 17)
+        Me.lbel2.TabIndex = 8
+        Me.lbel2.Text = "Result 2"
+        '
+        'lbel3
+        '
+        Me.lbel3.AutoSize = True
+        Me.lbel3.Location = New System.Drawing.Point(447, 285)
+        Me.lbel3.Name = "lbel3"
+        Me.lbel3.Size = New System.Drawing.Size(60, 17)
+        Me.lbel3.TabIndex = 9
+        Me.lbel3.Text = "Result 3"
+        '
+        'txt4
+        '
+        Me.txt4.Location = New System.Drawing.Point(516, 32)
+        Me.txt4.Multiline = True
+        Me.txt4.Name = "txt4"
+        Me.txt4.Size = New System.Drawing.Size(149, 42)
+        Me.txt4.TabIndex = 10
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(530, 159)
+        Me.txt1.Multiline = True
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(149, 42)
+        Me.txt1.TabIndex = 11
+        '
+        'txt5
+        '
+        Me.txt5.Location = New System.Drawing.Point(530, 278)
+        Me.txt5.Multiline = True
+        Me.txt5.Name = "txt5"
+        Me.txt5.Size = New System.Drawing.Size(149, 42)
+        Me.txt5.TabIndex = 12
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(159, 207)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(233, 35)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Reverse"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(159, 321)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(233, 35)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "Reverse"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'll
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.txt5)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.txt4)
+        Me.Controls.Add(Me.lbel3)
+        Me.Controls.Add(Me.lbel2)
+        Me.Controls.Add(Me.lbel1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "ll"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents lbel1 As Label
+    Friend WithEvents lbel2 As Label
+    Friend WithEvents lbel3 As Label
+    Friend WithEvents txt4 As TextBox
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents txt5 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Reverse the title/Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class ll
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim input As String
+        Dim result As String
+        Dim len As Integer
+        Dim a, b As Integer
+
+
+        input = TextBox1.Text
+        len = input.Length
+        a = len - 1
+        For b = 0 To a
+
+            result = result & input.Chars(a)
+            a = a - 1
+            txt4.Text = result
+
+        Next
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim input As String
+        Dim result As String
+        Dim len As Integer
+        Dim a, b As Integer
+
+
+        input = TextBox2.Text
+        len = input.Length
+        a = len - 1
+        For b = 0 To a
+
+            result = result & input.Chars(a)
+            a = a - 1
+            txt1.Text = result
+
+        Next
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim input As String
+        Dim result As String
+        Dim len As Integer
+        Dim a, b As Integer
+
+
+        input = TextBox3.Text
+        len = input.Length
+        a = len - 1
+        For b = 0 To a
+
+            result = result & input.Chars(a)
+            a = a - 1
+            txt5.Text = result
+
+        Next
+
+    End Sub
+End Class
